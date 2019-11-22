99b0e17205b493bf5500f754976ed2863e23d7fb
jizezhang <jizez@uw.edu>
2019-11-21 21:39:00 -0800

    modify: changed dismod_input

--- code/dismod_input.py
+++ code/dismod_input.py
@@ -1,11 +1,15 @@
 import sys
 from cascade_at.settings import settings
-from cascade_at.collectors.measurement_inputs import MeasurementInputsFromSettings
+from cascade_at.collector.measurement_inputs import MeasurementInputsFromSettings
 
-def get_input_from_db(model_version_id: int):
-    sts = settings.settings_from_model_version_id(model_version_id, "dismod-at-dev")
+def get_input_from_db(model_version_id=None, settings_json=None):
+    sts = None
+    if model_version_id is not None:
+        sts = settings.settings_from_model_version_id(model_version_id, "dismod-at-dev")
+    elif settings_json is not None:
+        sts = settings.load_settings(settings_json)
     inputs = MeasurementInputsFromSettings(sts)
-    inputs.configure_inputs_for_dismod()
+    inputs.configure_inputs_for_dismod(sts)
 
     df = inputs.dismod_data
     return df
