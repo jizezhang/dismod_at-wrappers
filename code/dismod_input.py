8176c1eba0f1d1067ed884b846644b77689ca2fe
jizezhang <jizez@uw.edu>
2019-11-26 15:12:00 -0800

    misc: minor change to dismod_input

--- code/dismod_input.py
+++ code/dismod_input.py
@@ -12,15 +12,13 @@ def get_settings(model_version_id=None, settings_json=None):
         return settings.load_settings(BASE_CASE)
 
 def get_input_from_db(model_version_id=None, settings_json=None, location_ids=None):
-    #sts = settings.settings_from_model_version_id(model_version_id, "dismod-at-dev")
     sts = get_settings(model_version_id, settings_json)
     inputs = MeasurementInputsFromSettings(sts)
-    if location_ids is not None:
-        inputs.demographics.location_id = location_ids
     inputs.get_raw_inputs()
-    #inputs.configure_inputs_for_dismod(sts)
 
     df = inputs.data.raw[['location_name', 'location_id', 'sex', 'year_start', 'year_end', 
                           'age_start', 'age_end', 'measure', 'mean', 'standard_error']]
-    #df.rename(columns={"time_lower": "age_start", "time_upper": "age_end"}, inplace=True)
+    if location_ids is not None:
+        df = df[df['location_id'].isin(location_ids)]
+    
     return df
